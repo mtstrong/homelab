59344273e78d206c50a32ae1e63582d8c70b4f28
Matt Strong <swevo24@gmail.com>
2024-01-23 20:30:54 -0600

    add new files/updated files
    added all the services, updated the setup file, and added a git ignore file so my dumbass doesnt try to commit my api keys

---
+++ scripts/setup_install.sh
@@ -0,0 +1,31 @@
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+sudo groupadd -f docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo apt install nfs-common
+sudo mkdir /mnt/ws2022
+sudo mount 192.168.1.153:/ws2022 /mnt/ws2022
+docker volume create sabnzbd --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:/ws2022/homelab/git/homelab/compose/sabnbd/config
+docker volume create nzbget --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:/ws2022/homelab/git/homelab/compose/nzbget/config
+docker volume create dls --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:/ws2022/homelab/git/homelab/compose/nzbget/downloads
+docker volume create traefik --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:/ws2022/homelab/git/homelab/compose/traefik-app
+docker volume create sonarr --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:/ws2022/homelab/git/homelab/compose/sonarr/config
+docker volume create movies --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:"/ws2022/bluray rips"
+docker volume create tv-completed --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:"/ws2022/tv shows/completed"
+docker volume create tv-current --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:"/ws2022/tv shows/currently airing"
+docker volume create movies --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:"/ws2022/bluray rips"
+docker volume create books --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:"/ws2022/books"
+docker volume create downloads --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:"/ws2022/downloads"
+docker volume create audiobooks --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:"/ws2022/audiobooks"
+docker volume create podcasts --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:"/ws2022/podcasts"
+docker volume create netboot --driver local --opt type=nfs --opt o=addr=192.168.1.153,rw --opt device=:"/ws2022/netboot"
