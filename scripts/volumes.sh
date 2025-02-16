e0a4f4f544e1f88c4feb177284e6a21495643549
Matt Strong <swevo24@gmail.com>
2025-02-16 17:30:19 +0000

    add openaudible

--- scripts/volumes.sh
+++ scripts/volumes.sh
@@ -1,6 +1,7 @@
 docker volume rm movies
 docker volume rm tv
 docker volume rm audiobooks
+docker volume rm audiobook-uploads
 docker volume rm podcasts
 docker volume rm torrent-dls
 docker volume rm nzb-dls
@@ -15,6 +16,7 @@ docker volume rm book-plugins
 docker volume create movies --driver local --opt type=nfs --opt o=addr=192.168.1.38,rw --opt device=:"mnt/TrueNAS/movies"
 docker volume create tv --driver local --opt type=nfs --opt o=addr=192.168.1.38,rw --opt device=:"/mnt/TrueNAS/tv"
 docker volume create audiobooks --driver local --opt type=nfs --opt o=addr=192.168.1.38,rw --opt device=:"/mnt/TrueNAS/audiobooks"
+docker volume create audiobook-uploads --driver local --opt type=nfs --opt o=addr=192.168.1.38,rw --opt device=:"/mnt/TrueNAS/audiobook-uploads"
 docker volume create podcasts --driver local --opt type=nfs --opt o=addr=192.168.1.38,rw --opt device=:"/mnt/TrueNAS/podcasts"
 docker volume create torrent-dls --driver local --opt type=nfs --opt o=addr=192.168.1.38,rw --opt device=:"/mnt/TrueNAS/torrent-dls"
 docker volume create nzb-dls --driver local --opt type=nfs --opt o=addr=192.168.1.38,rw --opt device=:"/mnt/TrueNAS/nzb-dls"
