ffcc9d9e39abf5ba5aba620730c6a171730d8fe5
Matt Strong <swevo24@gmail.com>
2026-02-26 17:36:41 +0000

    config: update Proxmox host to bd790i.local and use ubuntu-cloud template

--- terraform/proxmox-modules/main.tf
+++ terraform/proxmox-modules/main.tf
@@ -15,38 +15,37 @@ provider "proxmox" {
   pm_tls_insecure = var.proxmox_tls_insecure
 }
 
-module "vms_web" {
-  source = "./modules/vm"
-  
-  count = var.create_web_vms ? 1 : 0
-
-  vm_name           = "k3s-web-node"
-  target_node       = var.target_node
-  vmid              = var.web_vm_id
-  clone_template    = var.clone_template
-  cores             = var.web_cores
-  memory            = var.web_memory
-  disk_size         = var.web_disk_size
-  bridge            = var.bridge_vlan0
-  cloud_init_user   = var.cloud_init_user
-  cloud_init_password = var.cloud_init_password
-  ipconfig          = var.web_ipconfig
-}
-
-module "vms_worker" {
-  source = "./modules/vm"
-  
-  for_each = var.create_worker_vms ? var.worker_vms : {}
-
-  vm_name           = each.value.name
-  target_node       = each.value.target_node
-  vmid              = each.value.vmid
-  clone_template    = var.clone_template
-  cores             = each.value.cores
-  memory            = each.value.memory
-  disk_size         = each.value.disk_size
-  bridge            = var.bridge_vlan0
-  cloud_init_user   = var.cloud_init_user
-  cloud_init_password = var.cloud_init_password
-  ipconfig          = each.value.ipconfig
-}
+# Example usage - uncomment to create VMs
+# module "vms_web" {
+#   source = "./modules/vm"
+#   
+#   vm_name           = "k3s-web-node"
+#   target_node       = var.target_node
+#   vmid              = var.web_vm_id
+#   clone_template    = var.clone_template
+#   cores             = var.web_cores
+#   memory            = var.web_memory
+#   disk_size         = var.web_disk_size
+#   bridge            = var.bridge_vlan0
+#   cloud_init_user   = var.cloud_init_user
+#   cloud_init_password = var.cloud_init_password
+#   ipconfig          = var.web_ipconfig
+# }
+# 
+# module "vms_worker" {
+#   source = "./modules/vm"
+#   
+#   for_each = var.worker_vms
+# 
+#   vm_name           = each.value.name
+#   target_node       = each.value.target_node
+#   vmid              = each.value.vmid
+#   clone_template    = var.clone_template
+#   cores             = each.value.cores
+#   memory            = each.value.memory
+#   disk_size         = each.value.disk_size
+#   bridge            = var.bridge_vlan0
+#   cloud_init_user   = var.cloud_init_user
+#   cloud_init_password = var.cloud_init_password
+#   ipconfig          = each.value.ipconfig
+# }
